f867a670941be29d1c970bc04bb6c30444a20bb4
Pig <pig.priv@gmail.com>
2020-10-15 02:50:07 +0800

    soong: Drop bootleggers soong product variables
     * This reverts commits c59e356, ce8c7b4, f6807aa in preparation
       for using Soong config variables as per Google's docs.
    
    Change-Id: Ice01dcd8da10e3e2c38394acd9fe9a9ccb1d9784

--- android/variable.go
+++ android/variable.go
@@ -19,9 +19,7 @@ import (
 	"reflect"
 	"runtime"
 	"strings"
-
-	"bootleggers/soong/android"
-
+	
 	"github.com/google/blueprint/proptools"
 )
 
@@ -133,9 +131,6 @@ type variableProperties struct {
 			Srcs         []string `android:"arch_variant"`
 			Exclude_srcs []string `android:"arch_variant"`
 		} `android:"arch_variant"`
-
-		// include Bootleggers variables
-		Bootleggers android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -341,9 +336,6 @@ type productVariables struct {
 	InstallExtraFlattenedApexes *bool `json:",omitempty"`
 
 	BoardUsesRecoveryAsBoot *bool `json:",omitempty"`
-
-	// include Bootleggers variables
-	Bootleggers android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
@@ -412,23 +404,15 @@ func VariableMutator(mctx BottomUpMutatorContext) {
 	}
 
 	variableValues := reflect.ValueOf(a.variableProperties).Elem().FieldByName("Product_variables")
-	valStruct := reflect.ValueOf(mctx.Config().productVariables)
 
-	doVariableMutation(mctx, a, variableValues, valStruct)
-}
-
-func doVariableMutation(mctx BottomUpMutatorContext, a *ModuleBase, variableValues reflect.Value, valStruct reflect.Value) {
 	for i := 0; i < variableValues.NumField(); i++ {
 		variableValue := variableValues.Field(i)
 		name := variableValues.Type().Field(i).Name
 		property := "product_variables." + proptools.PropertyNameForField(name)
 
 		// Check that the variable was set for the product
-		val := valStruct.FieldByName(name)
-		if val.IsValid() && val.Kind() == reflect.Struct {
-			doVariableMutation(mctx, a, variableValue, val)
-			continue
-		} else if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
+		val := reflect.ValueOf(mctx.Config().productVariables).FieldByName(name)
+		if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
 			continue
 		}
 
